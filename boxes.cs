349ebed743f347ac8c57a9d0a9daebe8cefe6de8
Anshika Tyagi <53554113+anshika-hash@users.noreply.github.com>
2022-07-19 17:38:37 +0530

    Create boxes.cs
    Boxes will put the item or not using recursion.

---
+++ boxes.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Programming
+{
+    class boxes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A No Of Boxes");
+            int Boxes = int.Parse(Console.ReadLine());
+            fun(Boxes, "");
+            Console.ReadLine();
+        }
+        public static void fun(int noBoxes, string asf)
+        {
+            if (noBoxes == 0)
+            {
+                Console.WriteLine(asf);
+                return;
+            }
+
+                fun(noBoxes-1, asf + "-");
+                fun(noBoxes-1, asf + "+");
+            
+
+        }
+    }
+}
